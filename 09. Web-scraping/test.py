494f48081916d1adedb570c61135a06bcdc5be52
nagarjuna-de <nagarjuna.de@gmail.com>
2022-02-08 07:51:03 +0100

    Update test.py

--- 09. Web-scraping/test.py
+++ 09. Web-scraping/test.py
@@ -1,3 +1,35 @@
-l = [1,2,3,4,5]
-for i in l:
-    print(i)
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get('https://forecast.weather.gov/MapClick.php?x=276&y=148&site=lox&zmx=&zmy=&map_x=276&map_y=148#.YgGVvLrMKUl')
+
+soup = BeautifulSoup(page.content,'html.parser')
+
+lists_days = []
+lists_desc = []
+
+days_list = soup.find_all('div',class_ ="tombstone-container" )
+for days in days_list:
+    day_time = days.p.text
+    lists_days.append(day_time)
+
+#print(lists_days)
+
+## short description
+
+days_desc = soup.find_all('div', class_ ="col-sm-10 forecast-text")
+for desc in days_desc:
+    short_desc = desc.text
+    lists_desc.append(short_desc)
+
+#print(lists_desc)
+
+
+
+
+
+
+
+
+
+
